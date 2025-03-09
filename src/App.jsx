b0265f27ffea2a1f0ac03f1cbe8ede474ef3d1c2
alejandro21-rock <alejandrodeangelis21@gmail.com>
2025-03-09 15:37:40 -0300

    creacion del módulo notes.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Note from "./components/Note";
-import axios from "axios";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -8,8 +8,8 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    axios.get("http://localhost:3001/notes").then((response) => {
-      setNotes(response.data);
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
     });
   }, []);
 
@@ -19,10 +19,25 @@ const App = () => {
       content: newNote,
       important: Math.random() < 0.5,
     };
-    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
-      setNotes(notes.concat(response.data));
-      setNewNote("");
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote(""); //noteService es el nombre del objeto creado en notes.js que contiene todas las funciones que comunican con el backend
     });
+  };
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id); // El método de array find() se usa para encontrar la nota que queremos modificar, y luego se asigna a la variable note
+    const changedNote = { ...note, important: !note.important }; //Creamos un nuevo objeto que es una copia exacta de la nota anterior, excepto por la propiedad important que cambiará su valor de true a false o veceversa.
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      .catch((error) => {
+        alert(`the note '${note.content}' was already deleted from server`);
+        console.log(error);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
   };
 
   const handleNoteChange = (event) => {
@@ -43,7 +58,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
---
+++ src/services/notes.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3002/notes";
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
+
+//Se asigna la promesa a la variable request y se llama a su método then
+
+export default {
+  getAll,
+  create,
+  update,
+};
