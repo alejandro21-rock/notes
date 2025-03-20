2c7b733f99bcb6b2d97ba52132ee5ed6fed363e6
alejandro21-rock <alejandrodeangelis21@gmail.com>
2025-03-20 12:07:58 -0300

    Agregando estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import Note from "./components/Note";
 import noteService from "./services/notes";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -28,15 +30,17 @@ const App = () => {
   const toggleImportanceOf = (id) => {
     const note = notes.find((n) => n.id === id); // El método de array find() se usa para encontrar la nota que queremos modificar, y luego se asigna a la variable note
     const changedNote = { ...note, important: !note.important }; //Creamos un nuevo objeto que es una copia exacta de la nota anterior, excepto por la propiedad important que cambiará su valor de true a false o veceversa.
-    noteService
-      .update(id, changedNote)
-      .then((returnedNote) => {
-        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
-      })
-      .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`);
-        console.log(error);
-        setNotes(notes.filter((n) => n.id !== id));
+    noteService.update(id, changedNote).then((returnedNote) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+    })
+    .catch((error) => {
+      setErrorMessage(
+        `Note '${note.content}' was already removed from server`
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+      setNotes(notes.filter((n) => n.id !== id))
       });
   };
 
@@ -51,6 +55,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           Show {showAll ? "important" : "all"}
